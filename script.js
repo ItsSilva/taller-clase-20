7ba2736ac4465d220dfc13e37e0171da4c715ce0
Juan S Silva <juanse909@hotmail.com>
2024-04-04 09:19:30 -0500

    mala referencia en los inputs antes de su evento submite corregido

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const render = () => {
     const formulario = document.querySelector("#formulario");
 
-    const nombre = document.querySelector("#nombre");
-    const apellido = document.querySelector("#apellido");
-    const edad = document.querySelector("#edad");
+    // const nombre = document.querySelector("#nombre");
+    // const apellido = document.querySelector("#apellido");
+    // const edad = document.querySelector("#edad");
+    // Forma anterior de obtener los valores de los inputs
     const parrafo = document.querySelector("#parrafo");
 
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        const nombreValue = nombre.value;
-        const apellidoValue = apellido.value;
-        const edadValue = edad.value;
+        const nombreValue = e.target.nombre.value;
+        const apellidoValue = e.target.apellido.value;
+        const edadValue = e.target.edad.value;
         parrafo.innerHTML = `El nombre de la persona es ${nombreValue}, su apellido es ${apellidoValue} y su edad es ${edadValue} años.`;
     });
 };
